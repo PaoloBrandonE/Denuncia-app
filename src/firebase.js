9dd55ab8157a69f6e41813d32dd81fcb2322829f
Brandon Paolo Estrada Cuyto <210928@unsaac.edu.pe>
2025-12-28 19:13:51 -0500

    versión final

--- src/firebase.js
+++ src/firebase.js
@@ -3,21 +3,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";         
 import { getFirestore } from "firebase/firestore";
 
-// Configuración de Firebase usando variables de entorno
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY || "dummy",
-  authDomain: process.env.REACT_APP_PROJECT_ID
-    ? `${process.env.REACT_APP_PROJECT_ID}.firebaseapp.com`
-    : "dummy.firebaseapp.com",
-  projectId: process.env.REACT_APP_PROJECT_ID || "dummy",
-  storageBucket: process.env.REACT_APP_PROJECT_ID
-    ? `${process.env.REACT_APP_PROJECT_ID}.appspot.com`
-    : "dummy.appspot.com",
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID || "dummy",
-  appId: process.env.REACT_APP_APP_ID || "dummy"
+  apiKey: "AIzaSyBpv9DmaOw71OuX81mepLpPA3jsnoQgabk",
+  authDomain: "desarrollosoftawre.firebaseapp.com",
+  projectId: "desarrollosoftawre",
+  storageBucket: "desarrollosoftawre.appspot.com",
+  messagingSenderId: "451680575470",
+  appId: "1:451680575470:web:eb9cf80178c10cd08678ff"
 };
 
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 
+// Exportar servicios
 export const auth = getAuth(app);
 export const db = getFirestore(app);
